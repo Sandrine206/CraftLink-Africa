82659e8544a7e7f936055975aa8c2af9b47af60f
Sandrine206 <sandrinetech@outlook.com>
2025-05-06 22:25:34 +0000

    Few lines attempt
    Please, what should i add or amend?
    Is JavaScript necessary here?
     should i fork the first page separately from the second   ?

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+//Populate order summary
+const orderSummary = document.getElementById("order-summary");
+//Get order number from Url parameter or storage
+const orderNumber = "ORD-00013";
+//Replace with actual order number
+document.getElementById("order-number").textContent = orderNumber;
+
+//Back to home button
