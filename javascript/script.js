f7dcf911dbdd4c355b42deeed55884e530b2121b
Sandrine206 <sandrinetech@outlook.com>
2025-05-07 20:37:40 +0000

    Confirmation and cancel order button
    confirmation and cancel buttons after ordering

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,2 @@
-//Populate order summary
-const orderSummary = document.getElementById("order-summary");
-//Get order number from Url parameter or storage
-const orderNumber = "ORD-00013";
-//Replace with actual order number
-document.getElementById("order-number").textContent = orderNumber;
 
-//Back to home button
+
